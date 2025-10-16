2ff0065adae60717de97c87f3a7bbef56267124d
WAW-Elektronik <142165587+WAW-Elektronik@users.noreply.github.com>
2025-10-16 20:04:35 +0700

    Add files via upload

---
+++ data/KataKerja/KataKerjaN4PilihanGandaGrammar.js
@@ -0,0 +1,232 @@
+window.questions = [ 
+  {  
+    "pertanyaan": "ます ながら merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": ["(Sambil) melakukan 2 hal aktivitas dalam 1 waktu", "(Mudah untuk...)", "(Susah untuk...)", "Bentuk perintah sopan (Dari orang tua kepada anak, Dari guru ke murid, Kalimat perintah petunjuk mengerjakan soal tertulis)"],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "ます やすい merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": ["(Sambil) melakukan 2 hal aktivitas dalam 1 waktu", "(Mudah untuk...)", "(Susah untuk...)", "Bentuk perintah sopan (Dari orang tua kepada anak, Dari guru ke murid, Kalimat perintah petunjuk mengerjakan soal tertulis)"],  
+    "jawaban_index": 1  
+  },  
+  {  
+    "pertanyaan": "ます にくい merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": ["(Sambil) melakukan 2 hal aktivitas dalam 1 waktu", "(Mudah untuk...)", "(Susah untuk...)", "Bentuk perintah sopan (Dari orang tua kepada anak, Dari guru ke murid, Kalimat perintah petunjuk mengerjakan soal tertulis)"],  
+    "jawaban_index": 2  
+  },  
+  {  
+    "pertanyaan": "ます なさい merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": ["(Sambil) melakukan 2 hal aktivitas dalam 1 waktu", "(Mudah untuk...)", "(Susah untuk...)", "Bentuk perintah sopan (Dari orang tua kepada anak, Dari guru ke murid, Kalimat perintah petunjuk mengerjakan soal tertulis)"],  
+    "jawaban_index": 3  
+  },  
+  {  
+    "pertanyaan": "~て しまいます merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Menyelesaikan suatu aktivitas dengan tuntas",
+      "Menyatakan aktivitas yang tidak sengaja terjadi, terlanjur terjadi atau disayangkan untuk terjadi",
+      "(Terlebih dahulu)",
+      "Mencoba melakukan suatu hal"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て しまいました merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Menyelesaikan suatu aktivitas dengan tuntas",
+      "Menyatakan aktivitas yang tidak sengaja terjadi, terlanjur terjadi atau disayangkan untuk terjadi",
+      "(Terlebih dahulu)",
+      "Mencoba melakukan suatu hal"
+    ],  
+    "jawaban_index": 1  
+  },  
+  {  
+    "pertanyaan": "~て おきます merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "(Terlebih dahulu)",
+      "Mencoba melakukan suatu hal",
+      "Memberikan/melakukan sebuah perbuatan dari saya kepada orang lain yang status sosialnya lebih tinggi",
+      "Menerima sebuah perbuatan dari orang lain yang status sosialnya lebih tinggi pada kita (imbuhan 'di-kan')"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て みます merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Mencoba melakukan suatu hal",
+      "(Terlebih dahulu)",
+      "Memberikan/melakukan sebuah perbuatan dari saya kepada orang lain yang status sosialnya lebih rendah",
+      "Pergi melakukan suatu hal sejenak kemudian kembali lagi"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て さしあげます merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Meberikan/melakukan sebuah perbuatan dari saya kepada orang lain yang status sosialnya lebih tinggi",
+      "Menerima sebuah perbuatan dari orang lain yang status sosialnya lebih tinggi pada kita (imbuhan 'di-kan')",
+      "Orang lain yang lebih tinggi status sosialnya melakukan perbuatan untuk saya (imbuhan 'me-kan')",
+      "Memberikan/melakukan sebuah perbuatan dari saya kepada orang lain yang status sosialnya lebih rendah"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て いただきます merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Menerima sebuah perbuatan dari orang lain yang status sosialnya lebih tinggi pada kita (imbuhan 'di-kan')",
+      "Meberikan/melakukan sebuah perbuatan dari saya kepada orang lain yang status sosialnya lebih tinggi",
+      "Orang lain yang lebih tinggi status sosialnya melakukan perbuatan untuk saya (imbuhan 'me-kan')",
+      "Memberikan/melakukan sebuah perbuatan dari saya kepada orang lain yang status sosialnya lebih rendah"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て くださいます merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Orang lain yang lebih tinggi status sosialnya melakukan perbuatan untuk saya (imbuhan 'me-kan')",
+      "Menerima sebuah perbuatan dari orang lain yang status sosialnya lebih tinggi pada kita (imbuhan 'di-kan')",
+      "Meberikan/melakukan sebuah perbuatan dari saya kepada orang lain yang status sosialnya lebih tinggi",
+      "Bentuk perintah yang lebih sopan dari te kudasai"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て やります merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Memberikan/melakukan sebuah perbuatan dari saya kepada orang lain yang status sosialnya lebih rendah",
+      "Orang lain yang lebih tinggi status sosialnya melakukan perbuatan untuk saya (imbuhan 'me-kan')",
+      "Bentuk perintah yang lebih sopan dari te kudasai",
+      "Pergi melakukan suatu hal sejenak kemudian kembali lagi"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て いただけませんか merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Bentuk perintah yang lebih sopan dari te kudasai",
+      "Menerima sebuah perbuatan dari orang lain yang status sosialnya lebih tinggi pada kita (imbuhan 'di-kan')",
+      "Mencoba melakukan suatu hal",
+      "(Terlebih dahulu)"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て きます merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Pergi melakukan suatu hal sejenak kemudian kembali lagi",
+      "Mencoba melakukan suatu hal",
+      "Memberikan/melakukan sebuah perbuatan dari saya kepada orang lain yang status sosialnya lebih rendah",
+      "Menyelesaikan suatu aktivitas dengan tuntas"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て、KK merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Melakukan aktivitas A, dengan melakukan aktivitas B",
+      "(Karena) sebuah sebab yang berhubungan dengan perasaan",
+      "Menyatakan aktivitas yang tidak sengaja terjadi",
+      "(Terlebih dahulu)"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~て、~ merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "(Karena) sebuah sebab yang berhubungan dengan perasaan",
+      "Melakukan aktivitas A, dengan melakukan aktivitas B",
+      "Pergi melakukan suatu hal sejenak kemudian kembali lagi",
+      "Menyelesaikan suatu aktivitas dengan tuntas"
+    ],  
+    "jawaban_index": 0  
+  },
+  {  
+    "pertanyaan": "ないで、~aktivitas B merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Melakukan aktivitas B dengan tanpa melakukan aktivitas A",
+      "Perubahan keadaan yang tadinya bisa menjadi tidak bisa",
+      "(Sambil) melakukan 2 hal aktivitas dalam 1 waktu",
+      "(Mudah untuk...)"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "なくなりました merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Perubahan keadaan yang tadinya bisa menjadi tidak bisa",
+      "Melakukan aktivitas B dengan tanpa melakukan aktivitas A",
+      "(Susah untuk...)",
+      "(Terlebih dahulu)"
+    ],  
+    "jawaban_index": 0  
+    },   
+  {  
+    "pertanyaan": "る+な merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Perintah larangan pendek",
+      "Perubahaan keadaan yang tadinya tidak bisa menjadi bisa",
+      "Melakukan suatu hal demi...(demi/untuk)",
+      "Melakukan suatu hal demi...(demi/untuk) (pola dengan kata benda)"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "る よいになります merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Perubahaan keadaan yang tadinya tidak bisa menjadi bisa",
+      "Perintah larangan pendek",
+      "Melakukan suatu hal demi...(demi/untuk)",
+      "Melakukan suatu hal demi...(demi/untuk) (pola dengan kata benda)"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "る ために merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Melakukan suatu hal demi...(demi/untuk)",
+      "Perintah larangan pendek",
+      "Perubahaan keadaan yang tadinya tidak bisa menjadi bisa",
+      "Melakukan suatu hal demi...(demi/untuk) (pola dengan kata benda)"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "KB + の + ために merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "Melakukan suatu hal demi...(demi/untuk) (pola dengan kata benda)",
+      "Melakukan suatu hal demi...(demi/untuk)",
+      "Perintah larangan pendek",
+      "Perubahaan keadaan yang tadinya tidak bisa menjadi bisa"
+    ],  
+    "jawaban_index": 0  
+  },
+  {  
+    "pertanyaan": "~た あとで merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "(Setelah)",
+      "(Baru saja)",
+      "(Sambil) melakukan 2 hal aktivitas dalam 1 waktu",
+      "(Mudah untuk...)"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "KB + の + あとで merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "(Setelah)",
+      "(Baru saja)",
+      "(Susah untuk...)", 
+      "(Terlebih dahulu)"
+    ],  
+    "jawaban_index": 0  
+  },  
+  {  
+    "pertanyaan": "~た ばかり merupakan kalimat dengan arti/fungsi sebagai",  
+    "opsi": [
+      "(Baru saja)",
+      "(Setelah)",
+      "(Susah untuk...)", 
+      "(Terlebih dahulu)"
+    ],  
+    "jawaban_index": 0  
+  },
+];
