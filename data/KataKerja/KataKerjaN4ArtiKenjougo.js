5c7c3a7263fcbce477439576abc601ee4cec6904
WAW-Elektronik <142165587+WAW-Elektronik@users.noreply.github.com>
2025-10-29 15:51:19 +0700

    Add files via upload

---
+++ data/KataKerja/KataKerjaN4ArtiKenjougo.js
@@ -0,0 +1,12 @@
+window.soalKuisArti = [
+    { pertanyaan: "Apa arti dari Kenjougo'まいります'?", jawaban: "ada, datang, pergi" },
+    { pertanyaan: "Apa arti dari Kenjougo'うかがいます'?", jawaban: "pergi, mendengar" },
+    { pertanyaan: "Apa arti dari Kenjougo'おります'?", jawaban: "ada" },
+    { pertanyaan: "Apa arti dari Kenjougo'いただきます'?", jawaban: "makan, minum" },
+    { pertanyaan: "Apa arti dari Kenjougo'おめにかかります'?", jawaban: "bertemu" },
+    { pertanyaan: "Apa arti dari Kenjougo'おっしゃいます'?", jawaban: "berkata" },
+    { pertanyaan: "Apa arti dari Kenjougo'ぞんじております'?", jawaban: "mengetahui, mengerti" },
+    { pertanyaan: "Apa arti dari Kenjougo'ぞんじません'?", jawaban: "tidak mengetahui" },
+    { pertanyaan: "Apa arti dari Kenjougo'はいけんします'?", jawaban: "melihat" },
+    { pertanyaan: "Apa arti dari Kenjougo'いたします'?", jawaban: "melakukan" },
+];
---
+++ data/KataKerja/KataKerjaN4ArtiSonkeigo.js
@@ -0,0 +1,10 @@
+window.soalKuisArti = [
+    { pertanyaan: "Apa arti dari Sonkeigo'いらっしゃいます'?", jawaban: "ada, datang, pergi" },
+    { pertanyaan: "Apa arti dari Sonkeigo'めしあがります'?", jawaban: "makan, minum" },
+    { pertanyaan: "Apa arti dari Sonkeigo'おあいになります'?", jawaban: "cocok" },
+    { pertanyaan: "Apa arti dari Sonkeigo'もうします'?", jawaban: "berkata" },
+    { pertanyaan: "Apa arti dari Sonkeigo'ごぞんじです'?", jawaban: "mengetahui, mengenal" },
+    { pertanyaan: "Apa arti dari Sonkeigo'おらんになります'?", jawaban: "melihat" },
+    { pertanyaan: "Apa arti dari Sonkeigo'なさいます'?", jawaban: "melakukan" },
+    { pertanyaan: "Apa arti dari Sonkeigo'くださいます'?", jawaban: "memberikan" },
+];
