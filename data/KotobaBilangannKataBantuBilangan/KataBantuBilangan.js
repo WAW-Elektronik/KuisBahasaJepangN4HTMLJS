5e579dd4e94d37362f08ef895bb6629b7032bd05
WAW-Elektronik <142165587+WAW-Elektronik@users.noreply.github.com>
2025-11-01 21:56:25 +0700

    Add files via upload

---
+++ data/KotobaBilangannKataBantuBilangan/KataBantuBilangan.js
@@ -0,0 +1,128 @@
+window.soalKataBantuBilangan = [
+  //BAB26
+  {
+    pertanyaan: "Kata bantu bilangan dari '台 (dai)' untuk?",
+    KBBPenyebutan: "unit,buah",
+    KBBObjek: "mesin, produk, industrial",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '番 (ban)' untuk?",
+    KBBPenyebutan: "nomor",
+    KBBObjek: "nomor urutan",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '枚 (mai)' untuk?",
+    KBBPenyebutan: "lembar",
+    KBBObjek: "benda yang tipis seperti kertas",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '度 (do)' untuk?",
+    KBBPenyebutan: "kali, derajat",
+    KBBObjek: "kali, derajat",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '円 (en)' untuk?",
+    KBBPenyebutan: "yen",
+    KBBObjek: "mata uang yen",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari 'ルピア (rupia)' untuk?",
+    KBBPenyebutan: "rupiah",
+    KBBObjek: "mata uang rupiah",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '個 (ko)' untuk?",
+    KBBPenyebutan: "buah",
+    KBBObjek: "benda yang tidak terlalu besar",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '回 (kai)' untuk?",
+    KBBPenyebutan: "kali",
+    KBBObjek: "jumlah kali",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '歳 (sai)' untuk?",
+    KBBPenyebutan: "umur",
+    KBBObjek: "umur",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '冊 (satsu)' untuk?",
+    KBBPenyebutan: "jilid",
+    KBBObjek: "buku, majalah",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '着 (chaku)' untuk?",
+    KBBPenyebutan: "helai, setel",
+    KBBObjek: "pakaian",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '頭 (too)' untuk?",
+    KBBPenyebutan: "ekor",
+    KBBObjek: "binatang besar (kecuali ikan, unggas)",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari 'ページ (peeji)' untuk?",
+    KBBPenyebutan: "halaman",
+    KBBObjek: "halaman buku",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '杯 (hai/pai/bai)' untuk?",
+    KBBPenyebutan: "gelas, cangkir",
+    KBBObjek: "minuman yang dituangkan dalam wadak",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '本 (hon/pon/bon)' untuk?",
+    KBBPenyebutan: "batang",
+    KBBObjek: "barang yang berbetuk panjang",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '匹 (hiki/piki/biki)' untuk?",
+    KBBPenyebutan: "ekor",
+    KBBObjek: "binatang yang tidak besar",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '階 (kai/gai)' untuk?",
+    KBBPenyebutan: "tingkat",
+    KBBObjek: "tingkat bangunan",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '足 (soku/zoku)' untuk?",
+    KBBPenyebutan: "pasang",
+    KBBObjek: "sepatu, sandal, kaus kaki, dll",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '軒 (ken/gen)' untuk?",
+    KBBPenyebutan: "buah, unit",
+    KBBObjek: "rumah",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari 'つ (tsu)' untuk?",
+    KBBPenyebutan: "buah",
+    KBBObjek: "barang",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '人 (ri/nin)' untuk?",
+    KBBPenyebutan: "orang",
+    KBBObjek: "orang",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '部屋 (heya/peya)' untuk?",
+    KBBPenyebutan: "kamar",
+    KBBObjek: "kamar",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '皿 (sara)' untuk?",
+    KBBPenyebutan: "piring",
+    KBBObjek: "piring",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '切れ (kire)' untuk?",
+    KBBPenyebutan: "iris",
+    KBBObjek: "irisan",
+  },
+  {
+    pertanyaan: "Kata bantu bilangan dari '月 (tsuki, ka-getsu)' untuk?",
+    KBBPenyebutan: "bulan",
+    KBBObjek: "bulan",
+  },
+];
---
+++ data/KotobaBilangannKataBantuBilangan/KotobaTanggal.js
@@ -0,0 +1,38 @@
+window.soalKuisArti = [ 
+ // ■ Penyebutan Tanggal (日 / にち)
+  { pertanyaan: "Apa arti dari 'nannichi'?", jawaban: "tanggal berapa" },
+  { pertanyaan: "Apa arti dari 'tsuitachi'?", jawaban: "tanggal 1" },
+  { pertanyaan: "Apa arti dari 'futsuka'?", jawaban: "tanggal 2" },
+  { pertanyaan: "Apa arti dari 'mikka'?", jawaban: "tanggal 3" },
+  { pertanyaan: "Apa arti dari 'yokka'?", jawaban: "tanggal 4" },
+  { pertanyaan: "Apa arti dari 'itsuka'?", jawaban: "tanggal 5" },
+  { pertanyaan: "Apa arti dari 'muika'?", jawaban: "tanggal 6" },
+  { pertanyaan: "Apa arti dari 'nanoka'?", jawaban: "tanggal 7" },
+  { pertanyaan: "Apa arti dari 'youka'?", jawaban: "tanggal 8" },
+  { pertanyaan: "Apa arti dari 'kokonoka'?", jawaban: "tanggal 9" },
+  { pertanyaan: "Apa arti dari 'tooka'?", jawaban: "tanggal 10" },
+  { pertanyaan: "Apa arti dari 'juuyokka'?", jawaban: "tanggal 14" },
+  { pertanyaan: "Apa arti dari 'hatsuka'?", jawaban: "tanggal 20" },
+  { pertanyaan: "Apa arti dari 'nijuuyokka'?", jawaban: "tanggal 24" },
+
+  { pertanyaan: "Apa arti dari 'juuichinichi'?", jawaban: "tanggal 11" },
+  { pertanyaan: "Apa arti dari 'juuninichi'?", jawaban: "tanggal 12" },
+  { pertanyaan: "Apa arti dari 'juusannichi'?", jawaban: "tanggal 13" },
+  { pertanyaan: "Apa arti dari 'juugonichi'?", jawaban: "tanggal 15" },
+  { pertanyaan: "Apa arti dari 'juurokunichi'?", jawaban: "tanggal 16" },
+  { pertanyaan: "Apa arti dari 'juushichinichi'?", jawaban: "tanggal 17" },
+  { pertanyaan: "Apa arti dari 'juuhachinichi'?", jawaban: "tanggal 18" },
+  { pertanyaan: "Apa arti dari 'juukunichi'?", jawaban: "tanggal 19" },
+
+  { pertanyaan: "Apa arti dari 'nijuuichinichi'?", jawaban: "tanggal 21" },
+  { pertanyaan: "Apa arti dari 'nijuuninichi'?", jawaban: "tanggal 22" },
+  { pertanyaan: "Apa arti dari 'nijuusannichi'?", jawaban: "tanggal 23" },
+  { pertanyaan: "Apa arti dari 'nijuugonichi'?", jawaban: "tanggal 25" },
+  { pertanyaan: "Apa arti dari 'nijuurokunichi'?", jawaban: "tanggal 26" },
+  { pertanyaan: "Apa arti dari 'nijuushichinichi'?", jawaban: "tanggal 27" },
+  { pertanyaan: "Apa arti dari 'nijuuhachinichi'?", jawaban: "tanggal 28" },
+  { pertanyaan: "Apa arti dari 'nijuukunichi'?", jawaban: "tanggal 29" },
+
+  { pertanyaan: "Apa arti dari 'sanjuunichi'?", jawaban: "tanggal 30" },
+  { pertanyaan: "Apa arti dari 'sanjuuichinichi'?", jawaban: "tanggal 31" }
+];
